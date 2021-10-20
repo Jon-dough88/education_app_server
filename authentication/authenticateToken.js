26cab0e9766123c921f5ea49acc9a9d3b85736e1
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-10-20 11:07:52 +0300

    Required jsonwebtoken (authenticateToken).

--- authentication/authenticateToken.js
+++ authentication/authenticateToken.js
@@ -1,7 +1,9 @@
+const jwt = require('jsonwebtoken');
+
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if(token == null) {
+    if(token === null) {
         return res.sendStatus(401).send('Access denied')
     } else {
          const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => { 
