da9dfd3197feecd2e476dafbcd709ff1b43ae111
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-12-22 15:50:30 +0200

    Fixed a typo at appRouter.use('/groups...).

--- routers/appRouter.js
+++ routers/appRouter.js
@@ -2,10 +2,10 @@ const express = require('express');
 const appRouter = express.Router();
 
 const userRouter = require('./userRouter');
-const groupRouter = require('./groupRouter');s
+const groupRouter = require('./groupRouter');
 
 appRouter.use('/users', userRouter);
-appRouter.user('/groups', groupRouter)
+appRouter.use('/groups', groupRouter);
 
 
 module.exports = appRouter;
