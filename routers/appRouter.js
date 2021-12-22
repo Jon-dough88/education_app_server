10df3d5ecd8599256cbdc6137213320c6ce9b237
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-12-22 15:49:17 +0200

    Added groupRouter at appRouter.

--- routers/appRouter.js
+++ routers/appRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const appRouter = express.Router();
 
 const userRouter = require('./userRouter');
- 
+const groupRouter = require('./groupRouter');s
 
 appRouter.use('/users', userRouter);
 appRouter.user('/groups', groupRouter)
