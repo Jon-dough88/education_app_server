d075b4f5e242fe5820019c52ea6c6e8b8257a4ee
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-12-22 15:42:09 +0200

    Added a groupRouter file and added a referenece to it in the appRouter.

--- routers/appRouter.js
+++ routers/appRouter.js
@@ -4,6 +4,7 @@ const appRouter = express.Router();
 const userRouter = require('./userRouter');
 
 appRouter.use('/users', userRouter);
+appRouter.user('/groups', groupRouters)
 
 
 module.exports = appRouter;
