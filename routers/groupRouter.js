5ed75a7fe3b7c8b7d338e6137b978b2ff2a2393c
Jon-dough88 <guy.goodman.rater@gmail.com>
2022-01-10 11:54:29 +0200

    Changed the route of the groupRouter post request back to /fetchAll.

--- routers/groupRouter.js
+++ routers/groupRouter.js
@@ -9,14 +9,14 @@ const Teacher = require('../models/teacherSchema');
 groupRouter.post('/fetchAll', async(req, res) => {
     
     try{
-        // const userId = req.body;
-        const {_id: userId} = req.params;
+        const {userId} = req.body;
+        // const {userId: userId} = req.params;
         
         // const [teacherUsername] = userName
         console.log(`The teacher's user id is: ${userId}`)
 
         // await Teacher.find(userName).then(result => {
-        await Teacher.findById(userId).then(result => {    
+        await Teacher.findById({userId}).then(result => {    
             console.log(`Result: ${result}`)
             // res.status(200).send(result)
         }).catch(err => {
